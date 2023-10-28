50c95c6a89314b9fb6cf9720625093aea0755473
Srushti Pawar <87080882+SRUSHTI2493@users.noreply.github.com>
2023-10-28 23:20:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,6 @@ streamlit.dataframe( my_data_rows )
 
 fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice)
+[11:18 PM] Vaibhav Khairnar :: Manschaft
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
